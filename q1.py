f99631509e04b85a691ab412484b31b5b5ae0389
Davi Cardoso <davir@kyros.com.br>
2021-05-14 16:53:38 -0300

    q1 1.1 e 1.2

--- q1.py
+++ q1.py
@@ -2,72 +2,72 @@ import matplotlib.pyplot as plt
 
 #1.1 método de Euler
 
+u = 14.19
+ca_0 = 5.1
+k1 = 14.75
+k2 = 14.75
+k3 = 2.28
 
-def f_a(ca ,cb):
-    ca_0 = 5.1
-    u = 14.19
-    k1 = k2 = 14.75
-    k3 = 2.28
+def f_a(ca, _, __):
     fca = u * (ca_0 - ca) - k1*ca - k3*(ca ** 2)
     return fca
 
-def f_b(ca,cb):
-    u = 14.19
-    k1 = k2 = 14.75
-    k3 = 2.28
-    fcb = - u * cb + k1 * ca - k2 * cb
+def euler(f, ta, tb, i, h):
+    x = ta
+    y = tb
+    k = 0
+    list_y = [i]
+    list_x = [ta]
+    while x < tb:
+        y = y + h*f(y, k, h)
+        x += h
+        k += 1
+
+        list_y.append(y)
+        list_x.append(x)
+
+    return list_x, list_y, k
+
+def f_b(cb, k, h):
+    ca = euler(f_a, 0, 0.3, 0, h)
+    cay = ca[1]
+    fcb = -u * cb + k1 * cay[k] - k2 * cb
     return fcb
 
 
-def euler(y0, x0, t_max, h, f):
-    t_e = []
-    y_e = []
 
-    ca = y0
-    cb = x0
+def rungekutta(f, ta, tb, i, h):
+    x = ta
+    y = tb
+    k = 0
+    list_y = [i]
+    list_x = [ta]
+    while x <= tb:
+        z1 = h * f(y, k, h)
+        z2 = h * f(y + z1, k, h)
+        y = y + (z1 + z2) / 2
+        x = x + h
+        k = k + 1
 
-    while cb < t_max + h:
-        t_e.append(ca)
-        y_e.append(cb)
+        list_y.append(y)
+        list_x.append(x)
 
-        ca += h * f(ca, cb)
-        cb += h
+    return list_x, list_y, k
 
-    return t_e, y_e
-
-
-def rungekutta(y0, x0, t_max, h, f):
-    t_rk = []
-    y_rk = []
-
-    ca_rk = y0
-    cb_rk = x0
-
-    while cb_rk < t_max + h:
-        t_rk.append(ca_rk)
-        y_rk.append(cb_rk)
-
-        f1 = f(cb_rk, ca_rk)
-        f2 = f(cb_rk + 0.5*h*f1, ca_rk + 0.5*h)
-        ca_rk += h * f2
-        cb_rk += h
-
-    return t_rk, y_rk
 
 #1.2 resoluções
 
-f_a(0,0)
-f_b(0,0)
 
 #T = 0.05
 
 #euler
-v_ca = euler(0, 0, 0.3, 0.05, f_a)[0]
-v_cb = euler(0, 0, 0.3, 0.05, f_b)[0]
-v_h = euler(0, 0, 0.3, 0.05, f_a)[1]
+v_ca = euler(f_a, 0, 0.3, 0, 0.05)[0]
+v_cb = euler(f_b, 0, 0.3, 0, 0.05)[0]
+v_ha = euler(f_a, 0, 0.3, 0, 0.05)[1]
+v_hb = euler(f_b, 0, 0.3, 0, 0.05)[1]
 
-graf_ca, = plt.plot(v_h, v_ca)
-graf_cb, = plt.plot(v_h, v_cb)
+graf_ca, = plt.plot(v_ca, v_ha)
+graf_cb, = plt.plot(v_cb, v_hb)
 
 plt.xlabel('Iterações')
 plt.title('Euler com T =0.05')
@@ -80,12 +80,13 @@ print("Valor Final Método de Euler CB(0.3) = " + str(v_cb[-1]))
 
 #rungekutta
 
-v_ca_rk = rungekutta(0, 0, 0.3, 0.05, f_a)[0]
-v_cb_rk = rungekutta(0, 0, 0.3, 0.05, f_b)[0]
-v_h_rk = rungekutta(0, 0, 0.3, 0.05, f_a)[1]
+v_ca_rk = rungekutta(f_a, 0, 0.3, 0, 0.05)[0]
+v_cb_rk = rungekutta(f_b, 0, 0.3, 0, 0.05)[0]
+v_ha_rk = rungekutta(f_a, 0, 0.3, 0, 0.05)[1]
+v_hb_rk = rungekutta(f_b, 0, 0.3, 0, 0.05)[1]
 
-graf_ca, = plt.plot(v_h_rk, v_ca_rk)
-graf_cb, = plt.plot(v_h_rk, v_cb_rk)
+graf_ca, = plt.plot(v_ca_rk, v_ha_rk)
+graf_cb, = plt.plot(v_cb_rk, v_hb_rk)
 
 plt.xlabel('Iterações')
 plt.title('Runge kutta com T =0.05')
@@ -99,12 +100,14 @@ print("Valor Final Método de Runge Kutta CB(0.3) = " + str(v_cb_rk[-1]))
 
 #euler
 
-v_ca = euler(0, 0, 0.3, 0.03, f_a)[0]
-v_cb = euler(0, 0, 0.3, 0.03, f_b)[0]
-v_h = euler(0, 0, 0.3, 0.03, f_a)[1]
+v_ca = euler(f_a, 0, 0.3, 0, 0.03)[0]
+v_cb = euler(f_b, 0, 0.3, 0, 0.03)[0]
+v_ha = euler(f_a, 0, 0.3, 0, 0.03)[1]
+v_hb = euler(f_b, 0, 0.3, 0, 0.03)[1]
 
-graf_ca, = plt.plot(v_h, v_ca)
-graf_cb, = plt.plot(v_h, v_cb)
+graf_ca, = plt.plot(v_ca, v_ha)
+graf_cb, = plt.plot(v_cb, v_hb)
+
 
 plt.xlabel('Iterações')
 plt.title('Euler com T =0.03')
@@ -114,12 +117,13 @@ plt.show()
 
 #rungekutta
 
-v_ca_rk = rungekutta(0, 0, 0.3, 0.03, f_a)[0]
-v_cb_rk = rungekutta(0, 0, 0.3, 0.03, f_b)[0]
-v_h_rk = rungekutta(0, 0, 0.3, 0.03, f_a)[1]
+v_ca_rk = rungekutta(f_a, 0, 0.3, 0, 0.03)[0]
+v_cb_rk = rungekutta(f_b, 0, 0.3, 0, 0.03)[0]
+v_ha_rk = rungekutta(f_a, 0, 0.3, 0, 0.03)[1]
+v_hb_rk = rungekutta(f_b, 0, 0.3, 0, 0.03)[1]
 
-graf_ca, = plt.plot(v_h_rk, v_ca_rk)
-graf_cb, = plt.plot(v_h_rk, v_cb_rk)
+graf_ca, = plt.plot(v_ca_rk, v_ha_rk)
+graf_cb, = plt.plot(v_cb_rk, v_hb_rk)
 
 plt.xlabel('Iterações')
 plt.title('Runge kutta com T =0.03')
@@ -130,12 +134,13 @@ plt.show()
 #T = 0.01
 
 #euler
-v_ca = euler(0, 0, 0.3, 0.01, f_a)[0]
-v_cb = euler(0, 0, 0.3, 0.01, f_b)[0]
-v_h = euler(0, 0, 0.3, 0.01, f_a)[1]
+v_ca = euler(f_a, 0, 0.3, 0, 0.01)[0]
+v_cb = euler(f_b, 0, 0.3, 0, 0.01)[0]
+v_ha = euler(f_a, 0, 0.3, 0, 0.01)[1]
+v_hb = euler(f_b, 0, 0.3, 0, 0.01)[1]
 
-graf_ca, = plt.plot(v_h, v_ca)
-graf_cb, = plt.plot(v_h, v_cb)
+graf_ca, = plt.plot(v_ca, v_ha)
+graf_cb, = plt.plot(v_cb, v_hb)
 
 plt.xlabel('Iterações')
 plt.title('Euler com T =0.01')
@@ -147,14 +152,13 @@ print("Valor Final Método de Euler CB(0.3) = " + str(v_cb[-1]))
 
 
 #rungekutta
+v_ca_rk = rungekutta(f_a, 0, 0.3, 0, 0.01)[0]
+v_cb_rk = rungekutta(f_b, 0, 0.3, 0, 0.01)[0]
+v_ha_rk = rungekutta(f_a, 0, 0.3, 0, 0.01)[1]
+v_hb_rk = rungekutta(f_b, 0, 0.3, 0, 0.01)[1]
 
-v_ca_rk = rungekutta(0, 0, 0.3, 0.01, f_a)[0]
-v_cb_rk = rungekutta(0, 0, 0.3, 0.01, f_b)[0]
-v_h_rk = rungekutta(0, 0, 0.3, 0.01, f_a)[1]
-
-graf_ca, = plt.plot(v_h_rk, v_ca_rk)
-graf_cb, = plt.plot(v_h_rk, v_cb_rk)
-
+graf_ca, = plt.plot(v_ca_rk, v_ha_rk)
+graf_cb, = plt.plot(v_cb_rk, v_hb_rk)
 plt.xlabel('Iterações')
 plt.title('Runge kutta com T =0.01')
 plt.legend((graf_ca, graf_cb), ('CA(t)', 'CB(t)'), loc='upper left')
@@ -164,3 +168,4 @@ print("Valor Final Método de Runge Kutta CA(0.3) = " + str(v_ca_rk[-1]))
 print("Valor Final Método de Runge Kutta CB(0.3) = " + str(v_cb_rk[-1]))
 
 
+#1.3
